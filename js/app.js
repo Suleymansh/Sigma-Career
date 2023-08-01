94e5fa669208ffe6ede9fa672b26b0ad2f5b84fc
sheila <hasan@webluna.az>
2023-08-01 16:20:34 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Find all gallery items
+const galleryItems = document.querySelectorAll(
+  ".item.jumbotron-img-scroll-item"
+);
+
+// Clone the gallery items
+// galleryItems.forEach((item) => {
+//   const clone = item.cloneNode(true);
+//   clone.classList.add("clone");
+//   item.parentNode.appendChild(clone);
+// });
